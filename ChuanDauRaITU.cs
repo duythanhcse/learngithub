c0418d4a73b8cfa1bb4fdd876a2fd37cdfd374b0
duythanhcse <duythanhcse@gmail.com>
2019-06-30 12:31:56 +0700

    Tui cập nhật mã nguồn
    Đây là 3 mã nguồn xử lý mới

---
+++ ChuanDauRaITU.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mytikas.classes
+{
+    public class ChuanDauRaITU
+    {
+        public int STT { get; set; }
+        public int Id { get; set; }
+        public string MaChuanDauRa { get; set; }
+        public string TenChuanDauRa { get; set; }
+        public string I { get; set; }
+        public string T { get; set; }
+        public string U { get; set; }
+        public string I_BoMon { get; set; }
+        public string T_BoMon { get; set; }
+        public string U_BoMon { get; set; }
+        public string I_GiangVien { get; set; }
+        public string T_GiangVien { get; set; }
+        public string U_GiangVien { get; set; }
+    }
+}
